e25a9fdb521b4c39216b7faad33ab11cdad573b6
Nick Jones <nickj918@hotmail.com>
2016-11-04 14:12:56 -0400

    Switch to previous UI (replacement). All connections re-added.

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -11,8 +11,10 @@ import UIKit
 class ViewController: UIViewController, UITextFieldDelegate {
     
     // MARK: Properties
+    
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
     
 
     override func viewDidLoad() {
@@ -40,7 +42,9 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     
     // MARK: Actions
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+    }
+    @IBAction func setDefaultLabelText(_ sender: AnyObject) {
         mealNameLabel.text = "Default Text"
     }
     
