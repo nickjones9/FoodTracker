d74fda6049ee0ac79ca59291b136b52ccc1c2191
Nick Jones <nickj918@hotmail.com>
2016-11-05 15:14:09 -0400

    Button pressed responds, for loop added around button aspects.

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -10,23 +10,30 @@ import UIKit
 
 class RatingControl: UIView {
     
+    // MARK: Properties
+    
+    var rating = 0
+    var ratingButtons = [UIButton]()
+    
     // MARK: Initialization
     
     required init?(coder aDecoder: NSCoder) {
          super.init(coder: aDecoder)
         
+        for _ in 0..<5 {
         let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
         button.backgroundColor = UIColor.red
+        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchDown)
         addSubview(button)
-    }
-    
-    override public var intrinsicContentSize: CGSize {
-        get {
-            return CGSize(width: 240, height: 44)
-            
         }
     }
-    
+
+    override public var intrinsicContentSize: CGSize {
+            get {
+    return CGSize(width: 240, height: 44)
+            }
+    }
+
     //Mark: Button Action
     
     func ratingButtonTapped(button: UIButton) {
