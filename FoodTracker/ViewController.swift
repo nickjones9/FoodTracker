e2713a6f0c393cf3c8901437edcdd5260f3400c5
Nick Jones <nickj918@hotmail.com>
2016-11-06 15:52:33 -0500

    UI cleaned up and "Set Default Text" label deleted.

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
     
 
     override func viewDidLoad() {
@@ -69,10 +70,6 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         imagePickerController.delegate = self
         
         present(imagePickerController, animated: true, completion: nil)
-    }
-    
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        mealNameLabel.text = "Default Text"
     }
     
 }
