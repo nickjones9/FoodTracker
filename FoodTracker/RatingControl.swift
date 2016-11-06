038eafc28805deeaa8c69cce7ea722deb62b8b4c
Nick Jones <nickj918@hotmail.com>
2016-11-06 15:32:26 -0500

    More literal values replaced with variables.

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -23,11 +23,14 @@ class RatingControl: UIView {
     
     override func layoutSubviews() {
         
-        var buttonFrame = CGRect(x: 0, y: 0, width: 44, height: 44)
+        // Set the button's width and height to a square the size of the frame's height.
+        let buttonSize = Int(frame.size.height)
+        
+        var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
         
         // Offset each button's origin by the length of the button plus spacing.
         for (index, button) in ratingButtons.enumerated() {
-            buttonFrame.origin.x = CGFloat(index * (44 + spacing))
+            buttonFrame.origin.x = CGFloat(index * (buttonSize + 5))
             button.frame = buttonFrame
         }
     }
@@ -37,17 +40,20 @@ class RatingControl: UIView {
          super.init(coder: aDecoder)
         
         for _ in 0..<starCount {
-        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+        let button = UIButton()
         button.backgroundColor = UIColor.red
         button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchDown)
         ratingButtons += [button]
         addSubview(button)
         }
     }
-
+    
     override public var intrinsicContentSize: CGSize {
             get {
-    return CGSize(width: 240, height: 44)
+                let buttonSize = Int(frame.size.height)
+                let width = (buttonSize * starCount) + (spacing * (starCount - 1))
+                
+                return CGSize(width: width, height: 44)
             }
     }
 
