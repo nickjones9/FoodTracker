16d8e1c544da10fc8f08b6d06f4f1e859d4a690a
Nick Jones <nickj918@hotmail.com>
2016-11-06 14:14:07 -0500

    Literal spacing values replaced with variables.

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -15,6 +15,10 @@ class RatingControl: UIView {
     var rating = 0
     var ratingButtons = [UIButton]()
     
+    let spacing = 5
+    let starCount = 5
+    
+    
     // MARK: Initialization
     
     override func layoutSubviews() {
@@ -23,7 +27,7 @@ class RatingControl: UIView {
         
         // Offset each button's origin by the length of the button plus spacing.
         for (index, button) in ratingButtons.enumerated() {
-            buttonFrame.origin.x = CGFloat(index * (44 + 5))
+            buttonFrame.origin.x = CGFloat(index * (44 + spacing))
             button.frame = buttonFrame
         }
     }
@@ -32,7 +36,7 @@ class RatingControl: UIView {
         
          super.init(coder: aDecoder)
         
-        for _ in 0..<5 {
+        for _ in 0..<starCount {
         let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
         button.backgroundColor = UIColor.red
         button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchDown)
