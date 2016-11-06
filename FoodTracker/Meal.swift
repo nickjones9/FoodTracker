6bf8c52d68cdfb416f2209e6129bb11cfd43e7e7
Nick Jones <nickj918@hotmail.com>
2016-11-06 16:12:49 -0500

    Unit tests.

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,36 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Nicholas Jones on 2016-11-06.
+//  Copyright © 2016 Nicholas Jones. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    // MARK: Properties
+    
+    var name: String = ""
+    var photo: UIImage?
+    var rating: Int = 0
+    
+    // MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        // Initialization should fail if there is no name or if the rating is negative.
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+    }
+    
+    
+    
+    
+    
+}
