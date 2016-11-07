e575fe13552e44485325bec5a357cd1641e7b2c6
Nick Jones <nickj918@hotmail.com>
2016-11-06 19:51:54 -0500

    Fixed error, save button exit's.

--- MealTableViewController.swift
+++ MealTableViewController.swift
@@ -111,4 +111,16 @@ class MealTableViewController: UITableViewController {
     }
     */
 
+    @IBAction func unwindToMealList(sender: UIStoryboardSegue) {
+        
+        if let sourceViewController = sender.source as?
+            MealViewController, let meal = sourceViewController.meal {
+            
+            // Add a new meal.
+            let newIndexPath = IndexPath(row: meals.count, section: 0)
+            meals.append(meal)
+            tableView.insertRows(at: [newIndexPath], with: .bottom)
+        }
+    }
+    
 }
